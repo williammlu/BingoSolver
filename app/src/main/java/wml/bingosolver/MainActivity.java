74258186c1209417a4fcbe1edd103193d5dd98cc
williammlu <lu.william.m@gmail.com>
2016-01-03 23:01:36 -0800

    Changed color scheme.

--- app/src/main/java/wml/bingosolver/MainActivity.java
+++ app/src/main/java/wml/bingosolver/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Manage Boards"));
         tabLayout.addTab(tabLayout.newTab().setText("Entering Numbers"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Donate"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
